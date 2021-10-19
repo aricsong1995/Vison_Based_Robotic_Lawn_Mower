bc4e4af04fa1f5b35a7174af64e4e15263c55ac9
aricsong <aricsong1995@gmail.com>
2021-10-19 13:39:06 -0400

    Initial commit

---
+++ ball_track/lawn_mover.py
@@ -0,0 +1,8 @@
+from picar import front_wheels, back_wheels
+from picar.SunFounder_PCA9685 import Servo
+import picar
+from time import sleep
+import cv2
+import cv2.cv as cv
+import numpy as np
+import picar
